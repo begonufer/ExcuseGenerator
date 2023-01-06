39fe3f34bd6f93568f78dd8f6ee06539b9489c27
begonufer <begonufernandez@gmail.com>
2023-01-06 14:24:04 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function randomElement(arr) {
+    let auxArr = "";
+    for (let i = 0; i < 1; i++) {
+      auxArr += arr[Math.floor(Math.random() * arr.length)];
+    }
+    return auxArr;
+  }
+  return (
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when)
+  );
 };
+
+document.getElementById("excuse").innerHTML = onload();
